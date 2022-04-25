03dfad9937cc2916391d845008ce87b6ef779c89
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-04-26 00:03:46 +0500

    improvements

--- init.lua
+++ init.lua
@@ -1,28 +1,29 @@
 local toggle
-minetest.register_on_punchnode(function(pos, node)
+core.register_on_punchnode(function(pos, node)
 if not toggle then return end
-core.show_formspec('nodename','size[5,1]textarea[0,0;5,1;;Punched node name is:;\n'..node.name..']')
+core.show_formspec('nodename','size[5,1]field[0.3,0.3;5,1;nname;Punched node name is:;'..node.name..']')
 toggle = nil
 end)
-minetest.register_chatcommand("nname", {
+core.register_chatcommand("nname", {
     description = "Check nodename",
     func = function(param)
 toggle = 1
 return true, 'Punch node to check its name'
 end})
 
-minetest.register_chatcommand("node", {
-    description = "Check unpointable node's name where you standing",
+core.register_chatcommand("pos", {
+    description = "Check unpointable nodename where you standing",
     func = function(param)
 local node = core.get_node_or_nil(core.localplayer:get_pos())
 if not node then return false, 'No node!' end
-core.display_chat_message('Name: "'..node.name..'" param1: '..node.param1..' param2: '..node.param2)
+local pos = core.pos_to_string(vector.round(core.localplayer:get_pos()))
+core.show_formspec('poscheck','size[5,4]field[0.3,0.3;5,1;pos;Position:;'..pos:gsub('[%(%)]','')..']field[0.3,1.3;5,1;node;Node:;'..node.name..']field[0.3,2.3;5,1;p1;Param1:;'..node.param1..']field[0.3,3.3;5,1;p2;Param2:;'..node.param2..']')
 end})
 
-minetest.register_chatcommand("witem", {
+core.register_chatcommand("witem", {
   description = "View wielded item name",
   func = function()
-core.display_chat_message('Wielded item is '..minetest.colorize('#FF0',core.localplayer:get_wielded_item():get_name()))
+core.display_chat_message('Wielded item is '..core.colorize('#FF0',core.localplayer:get_wielded_item():get_name()))
 end})
 
 local function rs(color,name) return color and core.colorize('#F00',name) or core.colorize('#0F0',name) end
@@ -39,7 +40,7 @@ local sinfo = core.colorize('#0FF','---Server info---\n'..si.address..'\n'
 ..rs(csmr.read_nodedefs,' NodeDefs ')..'|'
 ..rs(csmr.lookup_nodes,' LookupNodes')
 
-minetest.register_chatcommand("sinfo", {
+core.register_chatcommand("sinfo", {
   description = "View server info",
   func = function()
 core.display_chat_message(sinfo)

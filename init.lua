e45f526febe9e541c5ed9f61eebeeddd3841cc8d
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-01-12 19:53:46 +0500

    Able to specity pos in .pos cmd

--- init.lua
+++ init.lua
@@ -14,11 +14,13 @@ end})
 
 core.register_chatcommand("pos", {
   description = "Check unpointable nodename where you standing",
+  params = "[pos]",
   func = function(param)
-	local pos = core.pos_to_string(vector.round(core.localplayer:get_pos()))
+	local pos = core.string_to_pos(param) or vector.round(core.localplayer:get_pos())
+	local strpos = core.pos_to_string(pos)
 	local node = core.get_node_or_nil(pos)
 	if not node then return false, "No node!" end
-	core.show_formspec("poscheck","size[5,4]field[0.3,0.3;5,1;pos;Position:;"..pos:gsub("[%(%)]","").."]field[0.3,1.3;5,1;node;Node:;"..node.name.."]field[0.3,2.3;5,1;p1;Param1:;"..node.param1.."]field[0.3,3.3;5,1;p2;Param2:;"..node.param2.."]")
+	core.show_formspec("poscheck","size[5,4]field[0.3,0.3;5,1;pos;Position:;"..strpos:gsub("[%(%)]","").."]field[0.3,1.3;5,1;node;Node:;"..node.name.."]field[0.3,2.3;5,1;p1;Param1:;"..node.param1.."]field[0.3,3.3;5,1;p2;Param2:;"..node.param2.."]")
 end})
 
 core.register_chatcommand("witem", {
